4bcc19a3e1a20be0320b79e981a54a56f982b814
Jayden Sansom <jaydensansom@gmail.com>
2018-07-24 17:54:30 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+# if x % 3 = 0
+#   then print fizz
+  
+#   otherwise x / 5 = a whole number
+#     then print buzz
+    
+#   otherwise x / 5 and x / 3 = whole numbers
+#     then print fizzbuzz
+    
+#   else 
+#     print nil
